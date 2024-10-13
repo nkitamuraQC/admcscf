8187ecd5c44d64dedc22a5446e1ae4b99d518e21
Naoki Kitamura <qclove00@NaokinoMacBook-Pro.local>
2024-10-13 20:44:25 +0900

    rot

--- orbital_grad.py
+++ orbital_grad.py
@@ -1,16 +1,13 @@
 import jax
-from pyscfad import gto, scf, fci, ao2mo
-from pyscfad.fci.fci_slow import absorb_h1e, contract_2e, fci_ovlp, kernel
-from pyscfad import pytree
+from pyscfad import gto, scf, ao2mo
+from pyscfad.fci.fci_slow import kernel
 import jax.numpy as jnp
-import numpy as np
 from math import factorial
 from pyscfad.tools import rotate_mo1
 from scipy.optimize import minimize
 from jax import value_and_grad
 from functools import reduce
-
-
+import numpy as np
 
 
 class CASSCF_Energy:
@@ -22,17 +19,16 @@ class CASSCF_Energy:
             nao = self.mol.nao
             assert nao == self.nmo
             size = nao * (nao - 1) // 2
-            self.x = np.zeros((size,))
+            self.x = jnp.zeros((size,))
         self.mo = rotate_mo1(mf.mo_coeff, self.x)
-        mo = self.mo
         self.mf = mf
         self.mol = mf.mol
         self.norb = mf.mo_coeff.shape[0]
         norb = self.norb
         self.nelec = mf.mol.nelectron
         hcore = mf.get_hcore()
-        self.int1e = jnp.einsum("ia,jb,ij->ab", mo, mo, hcore)
-        self.int2e = ao2mo.incore.full(mf._eri, mo)
+        self.int1e = jnp.einsum("ia,jb,ij->ab", self.mo, self.mo, hcore)
+        self.int2e = ao2mo.incore.full(mf._eri, self.mo)
         self.r0 = jnp.zeros((norb, norb))
         
         self.ncas = ncas
@@ -66,9 +62,9 @@ class CASSCF_Energy:
             return e[1] - e[0]
 
     def get_cas(self):
-        if mo_coeff is None: mo_coeff = self.mf.mo_coeff
-        if ncas is None: ncas = self.ncas
-        if ncore is None: ncore = self.ncore
+        mo_coeff = self.mf.mo_coeff
+        ncas = self.ncas
+        ncore = self.ncore
         mo_core = mo_coeff[:,:ncore]
         mo_cas = mo_coeff[:,ncore:ncore+ncas]
 
@@ -77,23 +73,27 @@ class CASSCF_Energy:
         if mo_core.size == 0:
             corevhf = 0
         else:
-            core_dm = np.dot(mo_core, mo_core.conj().T) * 2
+            core_dm = jnp.dot(mo_core, mo_core.conj().T) * 2
             corevhf = self.mf.get_veff(self.mol, core_dm)
-            energy_core += np.einsum('ij,ji', core_dm, hcore).real
-            energy_core += np.einsum('ij,ji', core_dm, corevhf).real * .5
-        h1eff = reduce(np.dot, (mo_cas.conj().T, hcore+corevhf, mo_cas))
+            energy_core += jnp.einsum('ij,ji', core_dm, hcore).real
+            energy_core += jnp.einsum('ij,ji', core_dm, corevhf).real * .5
+        h1eff = reduce(jnp.dot, (mo_cas.conj().T, hcore+corevhf, mo_cas))
         h2eff = self.int2e[ncore:ncore+ncas, ncore:ncore+ncas, ncore:ncore+ncas, ncore:ncore+ncas]
         return h1eff, h2eff, energy_core
     
 
 class CASSCF:
-    def __init__(self, mol, ncas, nelecas):
+    def __init__(self, mol, ncas, nelecas, elems=None):
         self.mol = mol
-        self.mf = scf.RHF(mol)
-        self.mf.kernel()
         self.ncas = ncas
         self.nelecas = nelecas
+        self.elems = elems
+        self.basis = "sto-3g"
         
+    def run_HF(self):
+        self.mf = scf.RHF(self.mol)
+        self.mf.kernel()
+        return
 
     def energy(self, x0, mf):
         myci = CASSCF_Energy(mf, x=jnp.asarray(x0), ncas=self.ncas, nelecas=self.nelecas)
@@ -107,31 +107,10 @@ class CASSCF:
     def opt_mo(self):
         nao = self.mf.mol.nao
         size = nao * (nao - 1) // 2
-        x0 = np.zeros((size,))
+        x0 = jnp.zeros((size,))
         options = {"gtol": 1e-6}
-        res = minimize(self.optimize_mo_fun, x0, args=(self.mf, ), jac=True, method="BFGS", options=options)
-        e = self.optimize_mo_fun(res.x, self.mf)[0]
+        res = minimize(self.optimize_mo_fun, x0, jac=True, method="BFGS", options=options)
+        e = self.optimize_mo_fun(res.x)[0]
         self.x = res.x 
         return e
-    
-    def energy_ci(self, mol):
-        nao = mol.nao
-        size = nao * (nao - 1) // 2
-        x0 = np.zeros((size,))
-        mf = scf.RHF(mol)
-        mf.kernel()
-        myci = CASSCF_Energy(mf, x=jnp.asarray(x0), ncas=self.ncas, nelecas=self.nelecas)
-        e = myci.casscf_energy3(nroots=2)
-        return e
-
-    def optimize_ci_fun(self, mol):
-        f, g = value_and_grad(self.energy_ci)(mol)
-        return (jnp.array(f), jnp.array(g))
-    
-    def opt_ci(self, mol):
-        res = minimize(self.optimize_ci_fun, mol, jac=True, method="BFGS", options=options)
-        e = self.optimize_ci_fun(res.mol, self.mf)[0]
-        self.mol_opt = res.mol
-        return e
-    
 
--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import jax
 from pyscfad import gto, scf, fci, ao2mo
 from orbital_grad import CASSCF
+import pytest
+from jax import numpy as jnp
+import numpy as np
 
 def fci_energy(mol, nroots=1):
     mf = scf.RHF(mol)
@@ -8,38 +11,22 @@ def fci_energy(mol, nroots=1):
     e, fcivec = fci.solve_fci(mf, nroots=nroots)
     return e
 
-def write_xyz(mol, fname="out.xyz"):
-    txt = ""
-    txt += str(len(mol.atom))+"\n"
-    txt += "\n"
-    for at in mol.atom:
-        txt += f"{at[0]} {at[1]} {at[2]} {at[3]}\n"
-    wf = open(fname, "w")
-    wf.write(txt)
-    return
-
 def test_casscf():
     # molecular structure
     mol = gto.Mole()
     mol.verbose = 0
     mol.output = None#"out_h2o"
     mol.atom = [
-        ["C",  (0.000000,  0.000000,  0.000000)]
-        ["O",  (0.000000,  0.000000,  1.200000)]
-        ["H",  (0.000000,  0.947700, -0.634900)]
-        ["H",  (0.000000, -0.947700, -0.634900)]
+        ["H",  (0.000000,  0.000000,  0.000000)],
+        ["H",  (0.000000,  0.000000,  1.200000)],
     ]
     mol.basis = "sto-3g"
     mol.build()
 
 
     e_fci = fci_energy(mol)
-    casscf = CASSCF(mol)
+    casscf = CASSCF(mol, 2, 2)
+    casscf.run_HF()
     e = casscf.opt_mo()
     assert(abs(e_fci - e) < 1e-6)
-
-    casscf.opt_ci(mol)
-    write_xyz(casscf.mol_opt)
     return
-        
-    
